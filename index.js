007678fb0f837c0e0e3e9b1ded100c9d63e2040d
rahimshahad <rahim.shahad2@gmail.com>
2021-09-14 18:05:31 -0400

    commit tree 86f69677ba615415421e563447737f4f286778bf

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
 ];
 
 const titleCased = () => {
-  // use the .map method on the tutorials to return a new array
-  return tutorials
+    // use the .map method on the tutorials to return a new array
+    return tutorials.map(string => {
+        let words = string.split(' ')
+        let capWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        let result = capWords.join(' ')
+        return result
+    })
 }
